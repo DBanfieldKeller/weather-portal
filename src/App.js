47824ccdc95ccde4320a1a067fd0e63ae69e852f
Goat <dpajbk@gmail.com>
2023-03-15 14:58:57 -0400

    fixed useEffect

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import Header from './components/header/header';
 import CurrentWeather from './components/currentWeather/currentWeather';
 import getForecast from './utils/weatherAPI';
 import InputBar from './components/inputBar/inputBar';
+import userEvent from '@testing-library/user-event';
 
 
 function App() {
 
-  const [currentLocation, setCurrentLocation] = useState("New York")
+  const [currentLocation, setCurrentLocation] = useState("New York");
+  const [isImperial, setIsImperial] = useState(true);
+  const [weatherData, setWeatherData ] = useState({});
 
-  useEffect(()=>{
+  const weatherLookup = (location, unitValue) =>
     getForecast({
-      cityName: currentLocation,
-      imperial: true
-    })
-  })
+      cityName: location,
+      isImperial: unitValue
+    });
+  
+  useEffect(() => {
+    weatherLookup(currentLocation,isImperial);
+  }, [])
 
   // getForecast({
   //   cityName: "Stamford",
@@ -25,8 +31,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <InputBar />
-      <CurrentWeather />
+      <InputBar
+        value={""}/>
+      <CurrentWeather
+        weatherData= {weatherData} />
     </div>
   );
 }
--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -5,10 +5,10 @@ const apiKey = "b442f7cbd3dba47d0df28083d882bce6"
 
 export default async function getForecast(request) {
     try{
-        const units = request.imperial?"imperial":"metric"
-
+        const units = request.isImperial?"imperial":"metric"
         const response = await axios.get(`${URL}${request.cityName}&units=${units}&appid=${apiKey}`);
         console.log("response: ", response)
+        return response
     }catch(error) {
         console.log(error)
         return []
