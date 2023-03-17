da9f27216cab645114b9becfa98141161fcdce00
Goat <dpajbk@gmail.com>
2023-03-17 16:35:22 -0400

    fixed metric unit for windspeed

--- src/components/currentWeather/currentWeather.js
+++ src/components/currentWeather/currentWeather.js
@@ -8,11 +8,11 @@ export default function CurrentWeather(props) {
         <div>
             <div className="currentweather">
                 <h2>Current Weather</h2>
-                <h3>{props.weatherData.name}</h3>
+                <h3>{props.weatherData.name??"Loading"}</h3>
                 <hr/>
                 <p>image</p>
                 <p>Temp: {Math.round(props.weatherData.main?.temp)}°{props.isImperial?"F":"C"}</p>
-                <p>Wind Speed: {Math.round(props.weatherData.wind?.speed)} {props.isImperial?"mph":"kph"??"Loading"}</p>
+                <p>Wind Speed: {Math.round(props.weatherData.wind?.speed)} {props.isImperial?"mph":"m/s"??"Loading"}</p>
                 <p>Humidity: {props.weatherData.main?.humidity??"Loading"}%</p>
             </div>
         </div>
